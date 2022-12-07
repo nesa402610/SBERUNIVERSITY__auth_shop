7a787b3e88d00afbacd9d246df9ff8273d737584
nesa402610 <argyleferris@gmail.com>
2022-12-07 14:08:07 +0300

    Слайс юзера

---
+++ src/store/reducers/authSlice.js
@@ -0,0 +1,33 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {fetchUser} from "../actions/fetchUser";
+
+const initialState = {
+  user: {},
+  isAuth: false,
+  isLoading: false,
+  error: null
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+  },
+  extraReducers: {
+    [fetchUser.fulfilled.type]: (state, action) => {
+      state.isAuth = true
+      state.isLoading = false
+      state.user = action.payload
+    },
+    [fetchUser.pending.type]: (state) => {
+      state.isLoading = true
+    },
+    [fetchUser.rejected.type]: (state, action) => {
+      state.isLoading = false
+      state.error = action.payload
+    }
+  }
+})
+
+export default authSlice.reducer
+// export const {} = authSlice.actions
