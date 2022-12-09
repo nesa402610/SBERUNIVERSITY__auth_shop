f78f73d58c30908e78173f9f52c6637ec6591148
nesa402610 <argyleferris@gmail.com>
2022-12-09 11:24:32 +0300

    Новый редусер продуктов

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import {combineReducers} from "redux";
 import {configureStore} from "@reduxjs/toolkit";
 import thunkMiddleware from 'redux-thunk';
 import authSlice from "./reducers/authSlice";
+import productsSlice from "./reducers/productsSlice";
 
 const rootReducer = combineReducers({
-auth: authSlice
-})
+  auth: authSlice,
+  products: productsSlice
+});
 export const store = configureStore({
   reducer: rootReducer,
   middleware: [thunkMiddleware]
-})
+});
