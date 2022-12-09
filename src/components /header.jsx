330985c760d62f9e6f266575b121e859902d188d
nesa402610 <argyleferris@gmail.com>
2022-12-09 18:33:34 +0300

    Красоты навел чуть-чуть

--- src/components /header.jsx
+++ src/components /header.jsx
@@ -6,30 +6,27 @@ import {filterProducts} from "../store/reducers/productsSlice";
 const Header = () => {
   const dispatch = useDispatch();
 
-  const searchHandler = (e) => {
-    dispatch(filterProducts(e.target.value));
-  };
-
   return (
     <header className={'flex justify-between text-lg px-4 py-2 bg-neutral-800'}>
-      <div>
-        <NavLink to={'/'}>Главная</NavLink>
+      <div className="flex gap-2">
+        <NavLink className={'hover:text-neutral-300'} to={'/'}>Главная</NavLink>
+        <NavLink className={'hover:text-neutral-300'} to={'/posts'}>Посты</NavLink>
       </div>
       <div className={'flex flex-1 justify-center'}>
         <input className={'bg-neutral-700 w-1/2 px-4 focus-visible:outline outline-1 outline-neutral-400'}
                type="text"
                placeholder={"Поиск..."}
-               onChange={e => searchHandler(e)}
+               onChange={e => dispatch(filterProducts(e.target.value))}
         />
       </div>
       {localStorage.getItem('token') ?
-        <NavLink to={'/profile'}>
+        <NavLink className={'hover:text-neutral-300'} to={'/profile'}>
           Профиль
         </NavLink>
         :
         <div>
-          <NavLink to={'/signUp'}>Регистрация</NavLink>
-          <NavLink to={'/signIn'}>Авторизация</NavLink>
+          <NavLink className={'hover:text-neutral-300'} to={'/signUp'}>Регистрация</NavLink>
+          <NavLink className={'hover:text-neutral-300'} to={'/signIn'}>Авторизация</NavLink>
         </div>
       }
     </header>
