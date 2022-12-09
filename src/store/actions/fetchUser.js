aaffce426fc46becb114ca9aabfc299fac4a42f3
nesa402610 <argyleferris@gmail.com>
2022-12-09 11:22:51 +0300

    На классовый компонент перевел. Требования к коду....

--- src/store/actions/fetchUser.js
+++ src/store/actions/fetchUser.js
@@ -1,10 +1,10 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import axios from "axios";
+import {api} from "../../APIs/API";
 
 export const fetchUser = createAsyncThunk(
   'user/fetchAuth',
   async (_, thunkAPI) => {
-    const response = await axios.get('https://api.react-learning.ru/v2/sm8/users/me')
+    const response = await api.getUserData()
     return response.data;
   }
 )
