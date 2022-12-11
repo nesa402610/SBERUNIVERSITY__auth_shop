0c92b669d79576db065a4abf8012ca0af6e250b4
nesa402610 <argyleferris@gmail.com>
2022-12-11 13:24:02 +0300

    Интерфейсы

---
+++ src/types.ts
@@ -0,0 +1,48 @@
+export interface IProduct {
+  available: boolean
+  pictures: string
+  likes: string[]
+  reviews: []
+  tags: string[]
+  isPublished: boolean
+  _id: string
+  name: string
+  author: IAuthor
+  price: number
+  discount: number
+  stock: number
+  wight: string
+  description: string
+  created_at?: string
+  updated_at?: string
+  __v?: number
+}
+
+export interface IAuthor {
+  name: string
+  about: string
+  avatar: string
+  _id: string
+  email: string
+  __v?: number
+}
+export interface IUser {
+  name: string
+  about: string | ''
+  avatar: string
+  _id?: string
+  email?: string
+  __v?: number
+}
+export interface IPost {
+  title: string
+  text: string
+  image?: string
+  tags?: string[]
+  likes?: []
+  comments?: []
+  isPublished?: boolean
+  _id?: string
+  author?: string
+  created_at?: string
+}
