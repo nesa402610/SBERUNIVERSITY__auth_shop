5b191f162407ae8350b48ea5bc6da1f95a4a6b80
nesa402610 <argyleferris@gmail.com>
2022-12-14 16:18:05 +0300

    Новые роуты

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Outlet, Route, Routes} from "react-router-dom";
 import SignIn from "./signIn";
 import SignUp from "./signUp";
 import HomePage from "./homePage";
@@ -7,6 +7,7 @@ import ProfilePage from "./profilePage";
 import PostsPage from "./postsPage";
 import PasswordReset from "./passwordReset";
 import ProductsPage from "./ProductsPage";
+import ProductDetails from "./ProductDetails";
 
 const Index = () => {
   return (
@@ -14,7 +15,10 @@ const Index = () => {
       <Route path={'/'} element={<HomePage/>}/>
       <Route path={'/profile'} element={<ProfilePage/>}/>
       <Route path={'/posts'} element={<PostsPage/>}/>
-      <Route path={'/catalog'} element={<ProductsPage/>}/>
+      <Route path={'/catalog'} element={<Outlet/>}>
+        <Route path={''} element={<ProductsPage/>}/>
+        <Route path={':productID'} element={<ProductDetails/>}/>
+      </Route>
       <Route path={'/signIn'} element={<SignIn/>}/>
       <Route path={'/signUp'} element={<SignUp/>}/>
       <Route path={'/reset-password'} element={<PasswordReset/>}/>
