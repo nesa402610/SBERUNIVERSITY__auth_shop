ef239db5afa7a49b915b9b9a719dbc93622835a6
nesa402610 <argyleferris@gmail.com>
2022-12-20 14:08:16 +0300

    Fixed теперь

--- src/components /posts/imagePreview_Modal.tsx
+++ src/components /posts/imagePreview_Modal.tsx
@@ -10,7 +10,7 @@ const ImagePreviewModal: FC<ImagePreviewModalProps> = ({isModal, setIsModal, ima
   return (
     <>
       {isModal &&
-        <div className={'flex justify-center items-center absolute bg-neutral-900/80 backdrop-blur-lg w-screen top-0 h-screen z-50'} onClick={() => setIsModal(false)}>
+        <div className={'flex justify-center items-center fixed bg-neutral-900/80 backdrop-blur-lg w-screen top-0 h-screen z-50'} onClick={() => setIsModal(false)}>
           <div className={' p-10 flex items-center justify-center'}>
             <img className={'h-[90vh] object-contain'} src={image} alt=""/>
           </div>
