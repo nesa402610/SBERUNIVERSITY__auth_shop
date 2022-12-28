fbc2fae319f046f7664d772ff693685e0a515438
nesa402610 <argyleferris@gmail.com>
2022-12-28 11:48:05 +0300

    Только выбранные товары к оформлению

--- src/router/CartPage.tsx
+++ src/router/CartPage.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {useAppDispatch, useAppSelector} from "../hooks/redux";
 import {decrementCart, incrementCart, removeItem} from "../store/reducers/cartSlice";
 import {ICart} from "../types";
@@ -6,11 +6,14 @@ import {fetchProducts} from "../store/actions/fetchProducts";
 
 
 const CartPage: FC = () => {
+  const [toBuy, setToBuy] = useState<string[]>([]);
   const {cart} = useAppSelector(state => state.cart)
   const dispatch = useAppDispatch()
 
-  const incrementHandler = (id: string) => {
-    dispatch(incrementCart(id))
+  const incrementHandler = (item: ICart) => {
+    if (item.count < item.product.stock) {
+      dispatch(incrementCart(item.product._id))
+    }
   }
   const decrementHandler = (id: string, count: number) => {
     if (count === 1) {
@@ -19,13 +22,30 @@ const CartPage: FC = () => {
   }
   const getTotalPrice = () => {
     let price = 0
-    cart.map((item: ICart) => price += (item.product.price * item.count * (100 - item.product.discount) / 100))
+    cart.map((item: ICart) => {
+      if (toBuy.includes(item.product._id)) {
+        return price += (item.product.price * item.count * (100 - item.product.discount) / 100)
+      }
+      return item
+    })
     return price
   }
   const getTotalItems = () => {
     let items = 0
-    cart.map((item: ICart) => items += item.count)
+    cart.map((item: ICart) => {
+      if (toBuy.includes(item.product._id)) {
+        return items += item.count
+      }
+      return item
+    })
     return items
+  }
+  const setBuyHandler = (id: string) => {
+    if (toBuy.includes(id)) {
+      setToBuy(prevState => prevState.filter(arrID => arrID !== id))
+    } else {
+      setToBuy(prev => [id, ...prev])
+    }
   }
   useEffect(() => {
     // @ts-ignore доп запрос на случай обновления данных о товаре
@@ -38,7 +58,9 @@ const CartPage: FC = () => {
       <div className={'flex gap-8 flex-col md:flex-row'}>
         <div className={'flex flex-1 flex-col gap-4'}>
           {cart.map((item: ICart) =>
-            <div key={item.product._id} className={'bg-neutral-700 rounded-lg p-4 flex justify-between gap-36 xs:gap-4 md:flex-row xs:flex-col'}>
+            <div key={item.product._id}
+                 className={'bg-neutral-700 rounded-lg p-4 flex justify-between gap-36 xs:gap-4 md:flex-row xs:flex-col'}>
+              <input type="checkbox" className={'absolute'} onChange={() => setBuyHandler(item.product._id)}/>
               <div className={'flex gap-4'}>
                 <div className={'min-w-[150px] rounded-lg w-[150px] h-[150px] overflow-hidden flex justify-center bg-white'}>
                   <img className={'h-full'} src={item.product.pictures} alt=""/>
@@ -50,12 +72,13 @@ const CartPage: FC = () => {
               </div>
               <div className={'flex flex-col items-center justify-center'}>
                 <div className={'flex bg-neutral-800 rounded-lg overflow-hidden gap-2 items-center'}>
-                  <button className={'p-4 rounded-none hover:bg-neutral-600 transition-all'}
-                          onClick={() => incrementHandler(item.product._id)}>Добавить
+                  <button className={(item.product.stock === item.count ? 'bg-neutral-600 cursor-default ' : '') + 'p-4 rounded-none hover:bg-neutral-600 transition-all'}
+                          onClick={() => incrementHandler(item)}>Добавить
                   </button>
                   <span className={'p-4 whitespace-nowrap'}>{item.count} шт.</span>
                   <button className={'p-4 rounded-none hover:bg-neutral-600 transition-all'}
-                          onClick={() => decrementHandler(item.product._id, item.count)}>Убавить
+                          onClick={() => decrementHandler(item.product._id, item.count)}>
+                    {item.count === 1 ? 'Удалить' : 'Убавить'}
                   </button>
                 </div>
                 <div className={'flex flex-col gap-2 w-full'}>
@@ -77,7 +100,7 @@ const CartPage: FC = () => {
             <div className={'flex flex-col p-4'}>
               <div className={'flex justify-between gap-4 font-bold'}>
                 <div className={'flex flex-col'}>
-                  <span>Товаров: {cart.length} шт.</span>
+                  <span>Товаров: {toBuy.length} шт.</span>
                   <span className={'text-sm text-neutral-400'}> в количестве {getTotalItems()} шт.</span>
                 </div>
                 <span>{getTotalPrice()} рублей</span>
